86afc03d49e7096f15274a24d589db57dbf1a149
fborello-lambda <federicoborello@lambdaclass.com>
2024-02-21 18:04:41 -0300

    Add Tesla HTTP-client as dep

--- lib/lambcast_web/controllers/page_controller.ex
+++ lib/lambcast_web/controllers/page_controller.ex
@@ -1,9 +1,12 @@
 defmodule LambcastWeb.PageController do
   use LambcastWeb, :controller
+  use Tesla
+  Tesla.Builder.plug(Tesla.Middleware.BaseUrl, "https://hub.farcaster.standardcrypto.vc:2281/v1")
+  Tesla.Builder.plug(Tesla.Middleware.JSON)
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    {:ok, data} = Tesla.get("https://hub.farcaster.standardcrypto.vc:2281/v1/castsByFid?fid=2")
+    # render(conn, :home, layout: false)
+    text(conn, "#{data.body}")
   end
 end
--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Lambcast.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:tesla, "~> 1.8"}
     ]
   end
 
