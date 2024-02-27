91ca32b0e34524e9a0244706e6447dd27cb172a9
fborello-lambda <federicoborello@lambdaclass.com>
2024-02-27 16:53:14 -0300

    Add simple message page

--- lib/lambcast_web/controllers/page_controller.ex
+++ lib/lambcast_web/controllers/page_controller.ex
@@ -7,11 +7,28 @@ defmodule LambcastWeb.PageController do
       Rpc.get_request("userNameProofByName?name=vitalik.eth")
 
     messages = Rpc.get_request("castsByFid?fid=#{user["fid"]}" <> "&reverse=1")
-    IO.puts(messages["data"]["castAddBody"]["mentions"])
 
     conn
     |> assign(:user, user)
     |> assign(:messages, messages["messages"])
     |> render(:home)
+  end
+
+  def test() do
+    messages = Rpc.get_request("castsByFid?fid=#{5650}" <> "&reverse=1")
+
+    for msg <- messages["messages"] do
+      if !msg["data"]["castAddBody"]["parentCastId"]["fid"] do
+        interactions =
+          Farcaster.Rpc.get_request(
+            "reactionsByCast?target_fid=#{5650}" <>
+              "&target_hash=#{msg["hash"]}" <> "&reaction_type=1"
+          )
+
+        # Not efficient
+        # IO.puts(Enum.count(interactions["messages"]))
+        IO.puts(msg["data"]["castAddBody"]["text"])
+      end
+    end
   end
 end
