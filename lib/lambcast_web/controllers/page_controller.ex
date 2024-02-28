672b511990b238fcafcdb111eac982dbea94baaf
fborello-lambda <federicoborello@lambdaclass.com>
2024-02-27 21:49:54 -0300

    Add template

--- lib/lambcast_web/controllers/page_controller.ex
+++ lib/lambcast_web/controllers/page_controller.ex
@@ -1,34 +1,15 @@
 defmodule LambcastWeb.PageController do
   use LambcastWeb, :controller
-  alias Farcaster.Rpc
+  use LambcastWeb, :live_view
 
   def home(conn, _params) do
-    user =
-      Rpc.get_request("userNameProofByName?name=vitalik.eth")
-
-    messages = Rpc.get_request("castsByFid?fid=#{user["fid"]}" <> "&reverse=1")
-
-    conn
-    |> assign(:user, user)
-    |> assign(:messages, messages["messages"])
-    |> render(:home)
+    render(conn, :home, data: %{"search" => "search_data"}, layout: false)
   end
 
-  def test() do
-    messages = Rpc.get_request("castsByFid?fid=#{5650}" <> "&reverse=1")
-
-    for msg <- messages["messages"] do
-      if !msg["data"]["castAddBody"]["parentCastId"]["fid"] do
-        interactions =
-          Farcaster.Rpc.get_request(
-            "reactionsByCast?target_fid=#{5650}" <>
-              "&target_hash=#{msg["hash"]}" <> "&reaction_type=1"
-          )
-
-        # Not efficient
-        # IO.puts(Enum.count(interactions["messages"]))
-        IO.puts(msg["data"]["castAddBody"]["text"])
-      end
-    end
+  def home(conn, %{"search" => user}) do
+    Phoenix.PageController.redirect(
+      conn,
+      Phoenix.Controller.redirect(conn, to: ~p"/#{user}")
+    )
   end
 end
