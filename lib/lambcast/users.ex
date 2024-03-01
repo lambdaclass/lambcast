f489a584e4864fa50fe1840c4f77a1fe475a0c08
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-01 19:09:12 -0300

    Add user data as entries un user map

--- lib/lambcast/users.ex
+++ lib/lambcast/users.ex
@@ -25,12 +25,24 @@ defmodule Lambcast.Users do
   def get_user!(username) do
     fid = get_fid!(username)
 
-    data = get_user_data!(fid)
-
-    %{
-      username: username,
+    data = %{
       fid: fid,
-      data: data
+      username: username
     }
+
+    for data <- get_user_data!(fid), into: data do
+      {user_data_type_number_to_atom(data.type), data.value}
+    end
+  end
+
+  defp user_data_type_number_to_atom(type_number) do
+    case type_number do
+      1 -> :profile_picture
+      2 -> :display_name
+      3 -> :biography
+      5 -> :url
+      6 -> :preferred_username
+      _ -> :none
+    end
   end
 end
